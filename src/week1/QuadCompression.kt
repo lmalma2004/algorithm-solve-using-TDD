54568f24276135b6ec4d13257059a3cac1ec600a
lmalma2004 <lmalma2004@naver.com>
2020-10-19 19:08:02 +0900

    [#4] implement method

--- src/week1/QuadCompression.kt
+++ src/week1/QuadCompression.kt
@@ -7,14 +7,22 @@ class QuadCompression {
     }
 
     fun checkArea(area: Array<IntArray>): Boolean {
-        return true
+        return area.all {
+            it.contains(1) != it.contains(0)
+        }
     }
 
     fun compressArea(area: Array<IntArray>): Int {
+        if(area.all { it.contains(1) })
+            return 1
         return 0
     }
 
     fun divideArea(loc: Pair<Int, Int>, size: Int): Array<Pair<Int, Int>> {
-        return arrayOf()
+        val loc1 = Pair(loc.first, loc.second)
+        val loc2 = Pair(loc.first, loc.second + size / 2)
+        val loc3 = Pair(loc.first + size / 2, loc.second)
+        val loc4 = Pair(loc.first + size / 2, loc.second + size / 2)
+        return arrayOf(loc1, loc2, loc3, loc4)
     }
 }
