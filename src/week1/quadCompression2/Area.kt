5986418bc6c510b59203fe782fcd53c516ba83c1
lmalma2004 <lmalma2004@naver.com>
2020-10-20 18:28:57 +0900

    [#6] implement Area

--- src/week1/quadCompression2/Area.kt
+++ src/week1/quadCompression2/Area.kt
@@ -12,6 +12,23 @@ class Area {
     private lateinit var area: Array<IntArray>
 
     fun quadCompress(startPoint: Point, areaSize: Int): IntArray {
-        return intArrayOf()
+        val compressor = Compressor(area)
+
+        if(compressor.canCompress(startPoint, areaSize)) {
+            return compressor.compress(startPoint)
+        }
+
+        val divider = Divider()
+        val dividedAreaPoints = divider.divide(startPoint, areaSize)
+        val dividedAreaSize = divider.nextSize(areaSize)
+
+        val compressedArea = intArrayOf(0, 0)
+
+        dividedAreaPoints.forEach {
+            val partialCompressedArea = quadCompress(it, dividedAreaSize)
+            compressedArea.mapIndexed { i, value -> value + partialCompressedArea[i] }
+        }
+
+        return compressedArea
     }
 }
