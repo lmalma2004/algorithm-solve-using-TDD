520bac6c5d0759f200affe5c3afb3f5260a589d0
lmalma2004 <lmalma2004@naver.com>
2020-10-20 21:22:01 +0900

    [#6] create Compressor & Divider class

---
+++ src/week1/quadCompression2/Compressor.kt
@@ -0,0 +1,12 @@
+package week1.quadCompression2
+
+class Compressor(area: Array<IntArray>) {
+    fun canCompress(startPoint: Point, areaSize: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    fun compress(startPoint: Point): IntArray {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/week1/quadCompression2/Divider.kt
@@ -0,0 +1,12 @@
+package week1.quadCompression2
+
+class Divider {
+    fun divide(startPoint: Point, areaSize: Int): ArrayList<Point> {
+        TODO("Not yet implemented")
+    }
+
+    fun nextSize(areaSize: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+}
