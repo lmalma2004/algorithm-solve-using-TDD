885155671de50bc1fd312c3e53992d4156da9d42
lmalma2004 <lmalma2004@naver.com>
2020-10-20 23:03:23 +0900

    [#6] add Compressor test

--- src/main/kotlin/week1/quadCompression2/Compressor.kt
+++ src/main/kotlin/week1/quadCompression2/Compressor.kt
@@ -1,6 +1,11 @@
 package week1.quadCompression2
 
-class Compressor(area: Array<IntArray>) {
+class Compressor(val area: Array<IntArray>) {
+    companion object {
+        fun create(area: Array<IntArray>): Compressor {
+            TODO("Not yet implemented")
+        }
+    }
     fun canCompress(startPoint: Point, areaSize: Int): Boolean {
         TODO("Not yet implemented")
     }
