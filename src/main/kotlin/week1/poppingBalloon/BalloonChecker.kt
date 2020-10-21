d63d65ee7a176b054418cae67077cc30a79b4220
lmalma2004 <lmalma2004@naver.com>
2020-10-21 20:39:49 +0900

    [#9] add BalloonChecker test

--- src/main/kotlin/week1/poppingBalloon/BalloonChecker.kt
+++ src/main/kotlin/week1/poppingBalloon/BalloonChecker.kt
@@ -1,7 +1,7 @@
 package week1.poppingBalloon
 
 class BalloonChecker {
-    fun check(balloons: Array<Balloon>): Any {
+    fun check(balloons: Array<Balloon>): Array<Balloon> {
         TODO("Not yet implemented")
     }
 
