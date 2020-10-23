497bd32c2f861c0da575af93e871deb9b4be88f2
lmalma2004 <lmalma2004@naver.com>
2020-10-24 00:42:10 +0900

    [#12] implement Mover

--- src/main/kotlin/week1/towerOfHanoi/Mover.kt
+++ src/main/kotlin/week1/towerOfHanoi/Mover.kt
@@ -8,14 +8,52 @@ class Mover {
     }
 
     fun canMove(state: State, tower: Int): Boolean {
-        TODO("Not yet implemented")
+        for(i in state.towers.indices) {
+            if (i == tower) {
+                continue
+            }
+
+            if(state.towers[i].discs.empty()) {
+                return true
+            }
+
+            val movingDiskSize = state.towers[tower].discs.peek()
+            val targetDiskSize = state.towers[i].discs.peek()
+            if (movingDiskSize < targetDiskSize) {
+                return true
+            }
+        }
+
+        return false
     }
 
-    fun nextTowers(state: State): IntArray {
-        TODO("Not yet implemented")
+    fun nextTowers(state: State, tower: Int): IntArray {
+        val result = arrayListOf<Int>()
+
+        for(i in state.towers.indices) {
+            if (i == tower) {
+                continue
+            }
+
+            if(state.towers[i].discs.empty()) {
+                result.add(i)
+                continue
+            }
+
+            val movingDiskSize = state.towers[tower].discs.peek()
+            val targetDiskSize = state.towers[i].discs.peek()
+            if (movingDiskSize < targetDiskSize) {
+                result.add(i)
+            }
+        }
+
+        return result.toIntArray()
     }
 
     fun move(state: State, from: Int, to: Int): State {
-        TODO("Not yet implemented")
+        val movingDisk = state.towers[from].discs.pop()
+        state.towers[to].discs.push(movingDisk)
+        state.path.add(intArrayOf(from, to))
+        return state
     }
 }
