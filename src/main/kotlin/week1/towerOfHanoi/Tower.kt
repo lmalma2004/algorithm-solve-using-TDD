4969f67cd2836f01746564b37225c469966809ee
lmalma2004 <lmalma2004@naver.com>
2020-10-23 23:17:44 +0900

    [#12] add Tower test

--- src/main/kotlin/week1/towerOfHanoi/Tower.kt
+++ src/main/kotlin/week1/towerOfHanoi/Tower.kt
@@ -1,10 +1,11 @@
 package week1.towerOfHanoi
 
-class Tower(var n : Int) {
+import java.util.*
+
+data class Tower(var discs : Stack<Int>) {
     companion object {
         fun create(n: Int): Tower {
             TODO("Not yet implemented")
         }
     }
-
 }
