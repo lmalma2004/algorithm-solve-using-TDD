41a77945e2cba6a9a6e2292df4dd86303871bd90
lmalma2004 <lmalma2004@naver.com>
2020-10-23 23:53:30 +0900

    [#12] implement State

--- src/main/kotlin/week1/towerOfHanoi/State.kt
+++ src/main/kotlin/week1/towerOfHanoi/State.kt
@@ -1,17 +1,27 @@
 package week1.towerOfHanoi
 
-class State {
+class State(
+        val towers: Array<Tower>,
+        val path: ArrayList<IntArray> = arrayListOf()
+) {
     fun isFinish(): Boolean {
-        TODO("Not yet implemented")
+        if(towers[0].discs.size == 0 && towers[1].discs.size == 0) {
+            return true
+        }
+        return false
     }
 
     fun path(): Array<IntArray> {
-        TODO("Not yet implemented")
+        return path.toTypedArray()
     }
 
     companion object {
         fun of(towers: Array<Tower>): State {
-            TODO("Not yet implemented")
+            if(towers.size != 3) {
+                throw IllegalArgumentException()
+            }
+
+            return State(towers)
         }
     }
 }
