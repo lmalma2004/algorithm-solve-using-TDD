ec2fb451df2911f9797c30ff68743f43940a3c39
lmalma2004 <lmalma2004@naver.com>
2020-10-28 11:21:03 +0900

    [#16] implement HtmlParser

--- src/main/kotlin/week2/matchingScore/HtmlParser.kt
+++ src/main/kotlin/week2/matchingScore/HtmlParser.kt
@@ -1,15 +1,46 @@
 package week2.matchingScore
 
 object HtmlParser {
-    fun findWord(html: String, word: String): Double {
-        TODO("Not yet implemented")
-    }
-
-    fun findLinks(html: String) {
-        TODO("Not yet implemented")
-    }
 
     fun findUrl(html: String): String {
-        TODO("Not yet implemented")
+        var idx = html.indexOf("content=") + 17
+        var url = ""
+
+        while(html[idx] != '\"') {
+            url += html[idx]
+            idx++
+        }
+
+        return url
+    }
+
+    fun findWord(html: String, word: String): Double {
+        val upperCaseHtml = html.toUpperCase()
+
+        val regex = "([^A-Z])"
+        val arr = upperCaseHtml.split(regex.toRegex())
+
+        return arr.filter { it == word.toUpperCase() }.size.toDouble()
+    }
+
+    fun findLinks(html: String): ArrayList<String> {
+        var idx = html.indexOf("a href=")
+        val jumpSize = 16
+
+        val links = arrayListOf<String>()
+        while(idx != -1) {
+            idx += jumpSize
+
+            var link = ""
+            while(html[idx] != '\"') {
+                link += html[idx]
+                idx++
+            }
+            links.add(link)
+
+            idx = html.indexOf("a href=", idx)
+        }
+
+        return links
     }
 }
