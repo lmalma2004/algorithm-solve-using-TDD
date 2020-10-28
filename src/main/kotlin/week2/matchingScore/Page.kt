a7858bd22b04c660b5662639e656c53a8aafbd78
lmalma2004 <lmalma2004@naver.com>
2020-10-28 12:23:51 +0900

    [#16] fix PagesManager

--- src/main/kotlin/week2/matchingScore/Page.kt
+++ src/main/kotlin/week2/matchingScore/Page.kt
@@ -5,18 +5,7 @@ data class Page(val html: String,
                 var basicScore: Double = 0.0,
                 var linkScore: Double = 0.0,
                 var links: ArrayList<String> = arrayListOf(),
-): Comparable<Page> {
-    override fun compareTo(other: Page): Int {
-        val matchPoint = basicScore + linkScore
-        val otherMatchPoint = other.basicScore + other.linkScore
-
-        return when {
-            matchPoint > otherMatchPoint -> 1
-            matchPoint < otherMatchPoint -> -1
-            else -> idx - other.idx
-        }
-    }
-
+) {
     companion object {
         fun create(s: String, idx: Int): Page {
             return Page(s, idx)
--- src/main/kotlin/week2/matchingScore/PagesManager.kt
+++ src/main/kotlin/week2/matchingScore/PagesManager.kt
@@ -1,6 +1,7 @@
 package week2.matchingScore
 
 import java.util.*
+import kotlin.Comparator
 
 class PagesManager private constructor() {
 
@@ -21,8 +22,22 @@ class PagesManager private constructor() {
     }
 
     fun getPageIdxOfMaxScore(pages: SortedMap<String, Page>): Int {
-        pages.toSortedMap()
-        return pages[pages.firstKey()]!!.idx
+        val sortList = pages.toList()
+        Collections.sort(sortList, PageComparator())
+        return sortList.first().second.idx
+    }
+
+    class PageComparator: Comparator<Pair<String, Page>> {
+        override fun compare(o1: Pair<String, Page>, o2: Pair<String, Page>): Int {
+            val o1matchPoint = o1.second.basicScore + o1.second.linkScore
+            val o2MatchPoint = o2.second.basicScore + o2.second.linkScore
+
+            return when {
+                o1matchPoint > o2MatchPoint -> -1
+                o1matchPoint < o2MatchPoint -> 1
+                else -> o1.second.idx - o2.second.idx
+            }
+        }
     }
 
     companion object {
